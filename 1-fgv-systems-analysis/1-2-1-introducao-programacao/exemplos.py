d904c4b0a1143f701094b4b5fdecbd69c9e9b035
¨Samuel <sam_falcao@hotmail.com>
2025-05-14 21:59:12 -0300

    Exemplos da unidade 1

---
+++ 1-fgv-systems-analysis/1-2-1-introducao-programacao/exemplos.py
@@ -0,0 +1,73 @@
+# # exemplo 1
+
+# salario_base_alice = 1000
+# vendas_alice = 3950
+# comissao_alice = 0.20
+# salario_alice = salario_base_alice + (vendas_alice * comissao_alice)
+
+# salario_base_bob = 999
+# faturamento_bob = 20000
+# comissao_fat_bob = 0.05
+# salario_bob = salario_base_bob + (faturamento_bob * comissao_fat_bob)
+
+# aluguel = 900
+# alimentacao = 600
+# contas_casa = 300
+
+# custo_mensal = aluguel + alimentacao + contas_casa
+
+# dinheiro_sobra = salario_alice + salario_bob - custo_mensal
+
+# print(salario_alice)
+# print(salario_bob)
+# print(custo_mensal)
+
+# print(dinheiro_sobra)
+
+
+# # exemplo 2
+
+# s = 'rock'
+# t = 'climbing'
+
+# print('o' in s) # True
+# print(s + t) # rockclimbing
+# print(s * 4) # rockrockrockrock
+# print(len(t)) # 8
+# print('ro' in s) # True
+# print(s[2]) # c
+# print(t[-8]) # c
+
+# # exemplo 3
+
+# notas = [9, 7, 7, 10, 3, 9, 6, 6, 2]
+# print(notas)
+# print(max(notas))
+
+# print(notas.count(7))
+
+# print(len(notas))
+# notas.pop()
+# notas.append(4)
+
+
+# notas.sort()
+# print(notas)
+
+# print(sum(notas) / len(notas))
+
+# exemplo 4
+
+# import math
+
+# hipotenusa = math.sqrt((3 ** 2) + ( 4 ** 2))
+
+# print(hipotenusa)
+
+# boolean = hipotenusa > 5
+
+# print(boolean)
+
+# area_circulo = (math.pi * (10 ** 2))
+
+# print(area_circulo)
